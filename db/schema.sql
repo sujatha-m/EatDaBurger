3c140df1d4b32b2349ad0013951c7543fface8e0
sujatha <jhanavi.bhushan@gmail.com>
2020-05-08 19:57:37 -0400

    Mysql database created

---
+++ db/schema.sql
@@ -0,0 +1,11 @@
+DROP DATABASE IF EXISTS burgers_db;
+CREATE DATABASE burgers_db;
+USE burgers_db;
+
+CREATE TABLE burgers
+(
+	id INTEGER NOT NULL AUTO_INCREMENT,
+	burger_name VARCHAR(255) NOT NULL,
+	devoured BOOLEAN DEFAULT false,
+	PRIMARY KEY (id)
+);
---
+++ db/seed.sql
@@ -0,0 +1,11 @@
+USE burgers_db;
+
+INSERT INTO burgers (burger_name , devoured)
+VALUES ('Fish-o-Fillet', false)
+
+INSERT INTO burgers (burger_name , devoured)
+VALUES ('Chicken-sandwich', false)
+
+INSERT INTO burgers (burger_name , devoured)
+VALUES ('Beef Burger', false)
+
