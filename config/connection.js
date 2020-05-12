8d9f8815c0883bf861bd54447b0130192b4a57a3
sujatha <jhanavi.bhushan@gmail.com>
2020-05-12 01:24:08 -0400

    renamed connection variable due to heroku issue

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,12 @@
 //Set up MySQL connection.
 const mysql = require("mysql2");
 
-var connection;
+var dbconnection;
 
 if(process.env.JAWSDB_URL) {
-  connection = mysql.createConnection(process.env.JAWSDB_URL);
+  dbconnection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
-  connection = mysql.createConnection({
+  dbconnection = mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
@@ -16,13 +16,13 @@ if(process.env.JAWSDB_URL) {
 }
 
 // Make connection.
-connection.connect(function(err) {
+dbconnection.connect(function(err) {
   if (err) {
     console.error("error connecting: " + err.stack);
     return;
   }
-  console.log("connected as id " + connection.threadId);
+  console.log("connected as id " + dbconnection.threadId);
 });
 
 // Export connection for our ORM to use.
-module.exports = (connection).promise()
+module.exports = (dbconnection).promise()
